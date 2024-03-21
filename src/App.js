7e977ab8b9aa373ffea9d2ff78e5e053dadb54cc
Tosin Isijola <tosinisijola@Tosins-MacBook-Pro.local>
2024-03-21 02:04:14 -0400

    fixing specific path fail

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ import HomePage from "./pages/HomePage";
 import { AppProvider } from "./context/ThemeContext";
 import TermsOfService from "./pages/TermsOfService";
 import ContactUs from "./pages/ContactUs";
+import ErrorPage from "./pages/ErrorPage";
 
 function App() {
 	return (
 		<AppProvider>
 			<Routes>
-				<Route exact path="/" element={<HomePage />} />
 				<Route exact path="/privacy-policy" element={<PrivacyPolicy />} />
 				<Route exact path="/terms-of-service" element={<TermsOfService />} />
 				<Route exact path="/contact-us" element={<ContactUs />} />
+				<Route exact path="*" element={<ErrorPage />} />
+				<Route exact path="/" element={<HomePage />} />
 			</Routes>
 		</AppProvider>
 	);
---
+++ src/pages/ErrorPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../reusable-components/Layout";
+
+function ErrorPage() {
+	return (
+		<Layout>
+			<div className="mx-auto max-w-max-content-width h-[85vh] flex justify-center items-center">
+				Page Not Found
+			</div>
+		</Layout>
+	);
+}
+
+export default ErrorPage;
